d88f8a7d3ed10a9f1f2f4424256a394dd03f786b
David Hirvonen <dhirvonen@elucideye.com>
2015-09-03 10:33:15 -0400

    build script changes

--- bin/build-ios.sh
+++ bin/build-ios.sh
@@ -2,13 +2,12 @@
 
 TOOLCHAIN=ios-8-4-arm64
 
-build.py --toolchain ${TOOLCHAIN} --verbose --fwd \
+build.py --toolchain ${TOOLCHAIN} --verbose --fwd HUNTER_CONFIGURATION_TYPES=Release BUILD_QT=ON \
 --config Release \
 --framework \
 --install \
 --jobs 8 \
 --open \
---nobuild
 #--reconfig \
 #--clear
 
