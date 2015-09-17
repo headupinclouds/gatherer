710f039762257f1dcdb5bda9f5ae18610b8f1797
David Hirvonen <dhirvonen@elucideye.com>
2015-09-17 00:29:10 -0400

    Add platform specific package configurations

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -7,30 +7,29 @@ string(COMPARE EQUAL "${CMAKE_OSX_SYSROOT}" "iphoneos" is_ios)
 string(COMPARE EQUAL "${CMAKE_SYSTEM_NAME}" "Linux" is_linux)
 
 ### OpenCV
-# Note: This will be more complicated for the final release
-if(is_ios)
-  message("is_ios =========================================================================")
-  include(SetOpenCVCMakeArgs-iOS) 
-  set_opencv_cmake_args_ios()
-elseif(@ANDROID@)
-  message("ANDROID =========================================================================")
+if(@ANDROID@)
+  message("ANDROID =====================================================================")
   include(SetOpenCVCMakeArgs-android) 
   set_opencv_cmake_args_android()
+elseif(is_ios)
+  message("is_ios ======================================================================")
+  include(SetOpenCVCMakeArgs-iOS) 
+  set_opencv_cmake_args_ios()
 elseif(APPLE) 
-  message("APPLE =========================================================================")
+  message("APPLE =======================================================================")
   include(SetOpenCVCMakeArgs-osx)
   set_opencv_cmake_args_osx()
 elseif(is_linux)
-  message("is_linux =========================================================================")
+  message("is_linux ====================================================================")
   set(OPENCV_CMAKE_ARGS BUILD_SHARED_LIBS=OFF WITH_FFMPEG=OFF WITH_IPP=OFF) #TBD
 elseif(MSCV)
-  message("MSVC =========================================================================")
+  message("MSVC ========================================================================")
   set(OPENCV_CMAKE_ARGS BUILD_SHARED_LIBS=OFF WITH_FFMPEG=OFF WITH_IPP=OFF) #TBD
 endif()
 
 list(APPEND OPENCV_CMAKE_ARGS 
   BUILD_opencv_world=ON 
-  BUILD_EIGEN=OFF  ### for convenient linking
+  BUILD_EIGEN=OFF
   CMAKE_CXX_FLAGS=-Wno-c++11-narrowing
 )
 hunter_config(OpenCV VERSION ${HUNTER_OpenCV_VERSION} CMAKE_ARGS "${OPENCV_CMAKE_ARGS}")
