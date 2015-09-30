1378ba24105b956ec3d5193c9f9e911248fe3a92
David Hirvonen <dhirvonen@elucideye.com>
2015-09-30 15:36:13 -0400

    added build-android.sh script

---
+++ bin/build-android.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TOOLCHAIN=android-ndk-r10e-api-16-armeabi-v7a-neon-clang-35-hid
+
+build.py --toolchain ${TOOLCHAIN} --verbose --fwd HUNTER_CONFIGURATION_TYPES=Release BUILD_QT=ON ANDROID=TRUE \
+--config Release \
+--install \
+--strip \
+--clear \
+--jobs 8 
