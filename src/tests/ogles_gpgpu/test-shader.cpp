c5f24d6b1f9f248496d0f42d6ff1ef2351644da3
David Hirvonen <dhirvonen@elucideye.com>
2016-02-01 13:28:51 -0500

    added simple gtest for ogles_gpgpu shaders (skeleton)

--- src/tests/ogles_gpgpu/test-shader.cpp
+++ src/tests/ogles_gpgpu/test-shader.cpp
@@ -1,6 +1,12 @@
 #include <gtest/gtest.h>
 
+#include <GL/glew.h> // include GLEW and new version of GL on Windows
+#include <GLFW/glfw3.h>
+#include "GLContextWindow.h"
+
+#include "graphics/gatherer_graphics.h"
 #include "OGLESGPGPUTest.h"
+#include "graphics/Logger.h"
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
@@ -26,11 +32,21 @@ protected:
 	// Setup
 	OGLESGPGPUTest()
 	{
+        m_logger = gatherer::graphics::Logger::create("test-shader");
+        
 		// Load the ground truth data:
-		loadTruth();
+        image = loadImage(imageFilename);
+        
+        // TODO: we need to load ground truth output for each shader
+        // (some combinations could be tested, but that is probably excessive!)
+        truth = loadImage(truthFilename);
+        
+        m_context = std::make_shared<gatherer::graphics::GLContextWindow>(image.size(), "test-shader");
 
-		// Load sample input images
-		loadImages();
+        float resolution = 2.f;
+        void *glContext = nullptr;
+        m_pipeline = std::make_shared<gatherer::graphics::OEGLGPGPUTest>(glContext, resolution);
+        m_pipeline->setDoDisplay(true); // TODO: temporary display for debug
 	}
 
 	// Cleanup
@@ -44,22 +60,21 @@ protected:
 
 	// Called after destructor for each test
 	virtual void TearDown() {}
+    
+    static cv::Mat loadImage(const std::string &filename)
+    {
+        assert(!filename.empty());
+        cv::Mat image = cv::imread(filename, cv::IMREAD_COLOR);
+        assert(!image.empty() && image.type() == CV_8UC3);
+        
+        cv::Mat tmp;
+        cv::cvtColor(image, tmp, cv::COLOR_BGR2BGRA);
+        cv::swap(image, tmp);
+        return image;
+    }
 
-	// Utility methods:
-	void loadImages()
-	{
-		assert(imageFilename);
-		image = cv::imread(imageFilename, cv::IMREAD_COLOR);
-		assert(!image.empty());
-	}
-
-	void loadTruth()
-	{
-		assert(truthFilename);
-		truth = cv::imread(truthFilename, cv::IMREAD_COLOR);
-		assert(!image.empty());
-	}
-
+    std::shared_ptr<gatherer::graphics::GLContextWindow> m_context;
+    std::shared_ptr<spdlog::logger> m_logger;
     std::shared_ptr<gatherer::graphics::OEGLGPGPUTest> m_pipeline;
     
 	// Test images:
@@ -72,7 +87,18 @@ protected:
 
 TEST_F(OGLESGPGPUTest, grayscale)
 {
+    void * pixelBuffer = image.ptr();
+    bool useRawPixels = true;
+    m_pipeline->captureOutput(image.size(), pixelBuffer, useRawPixels, 0, GL_BGRA);
+    
+    cv::Mat result(image.size(), CV_8UC4);
+    m_pipeline->getOutputData(result.ptr());
+    
+    // TODO: compare result with expected ground truth result
 
+    cv::imshow("result", result);
+    m_context->swapBuffers();
+    cv::waitKey(2000);
 }
 
 END_EMPTY_NAMESPACE
